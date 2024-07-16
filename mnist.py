148e56567230635074c637a9bdefc86665f5fa7e
ethan <ethanrdibble@gmail.com>
2024-07-15 20:35:07 -0700

    lazy

--- mnist.py
+++ mnist.py
@@ -20,20 +20,85 @@ class Perceptron:
         self.output_weights = WEIGHT_SCALE * np.random.randn(output_units, hidden_units + 1)
 
     def activation(self, a):
-        # sigmoid function
+        """
+        Activation sigmoid function
+        """
         return 1 / (1 + math.exp(-a))
 
-    def output_error(self):
-        a = 1
+    def forward(self, output_activations, hidden_activations, x):
+        """
+        Forward phase
+        """
+        # forward phase
+        # hidden activations
+        for j in range(self.hidden_units):
+            # j-th hidden unit
+            a = np.dot(x, self.hidden_weights[j])
+            hidden_activations[j + 1] = self.activation(a)   # activation of hidden layer neurons
 
-    def hidden_error(self):
-        a = 1
+        hidden_activations[0] = 1.0     # bias 
 
-    def fix_weights(self):
-        a = 1
+        # output activations
+        for k in range(self.output_units):
+            # k-th output unit
+            a = np.dot(hidden_activations, self.output_weights[k])
+            output_activations[k] = self.activation(a)       # activation of 
+
+    def errors(self, output_activations, output_errors, hidden_activations, hidden_errors, target): 
+        """
+        Get errors
+        """
+        # not all fine and dandy
+        # output errors
+        for k in range(self.output_units):
+            t = 0.9 if k == target else 0.1
+            y = output_activations[k]
+            # be careful of sign
+            output_errors[k] = y * (1 - y) * (t - y)
+
+        # hidden errors
+        for j in range(self.hidden_units):
+            h = hidden_activations[j + 1]   # bias is index 0
+            # be careful of sign
+            sum = 0.0
+            for k in range(self.output_units):
+                sum += self.output_weights[k][j] * output_errors[k]
+            hidden_errors[j] = h * (1 - h) * sum
+
+    def deltas(self, output_errors, output_deltas, hidden_activations, hidden_errors, hidden_deltas, x):
+        """
+        Get weight deltas
+        """
+        # output deltas
+        for k in range(self.output_units):
+            # k-th output unit
+            for j in range(self.hidden_units + 1):
+                # j-th hidden unit
+                output_deltas[k][j] = (
+                    (self.learning_rate 
+                    * output_errors[k] 
+                    * hidden_activations[j]) 
+                    + (self.momentum 
+                    * output_deltas[k][j])
+                )
+
+        # hidden deltas
+        for j in range(self.hidden_units):
+            # j-th hidden unit
+            for i in range(self.input_size + 1):
+                # i-th input
+                hidden_deltas[j][i] = (
+                    (self.learning_rate 
+                    * hidden_errors[j]
+                    * x[i])
+                    + (self.momentum
+                    * hidden_deltas[j][i])
+                )
 
     def train(self, x_train, y_train, x_test, y_test, epochs):
         """
+        Train the model
+
         for each input vector:
             forwards phase:
                 - compute the activation of each neuron j in the hidden layer(s)
@@ -139,14 +204,56 @@ class Perceptron:
             train_accuracy = num_correct / len(x_train)
             train_accuracies.append(train_accuracy)
 
-            #test_accuracy = self.evaluate(x_test, y_test)
-            #test_accuracies.append(test_accuracy)
+            test_accuracy = self.evaluate(x_test, y_test)
+            test_accuracies.append(test_accuracy)
 
-            #print(f'Epoch {epoch} : Correct Train {num_correct:.0f} : Accuracy Train {train_accuracy:.4f} : Accuracy Test {test_accuracy:.4f}')
-            print(f'Epoch {epoch} : Correct Train {num_correct:.0f} : Accuracy Train {train_accuracy:.4f}')
+            print(f'Epoch {epoch} : Correct Train {num_correct:.0f} : Accuracy Train {train_accuracy:.4f} : Accuracy Test {test_accuracy:.4f}')
 
         return train_accuracies, test_accuracies
             
+    def evaluate(self, x_test, y_test):
+        """
+        Evaluate model on the test set
+        """
+        output_activations = np.zeros(self.output_units)        # output activations
+        hidden_activations = np.zeros(self.hidden_units + 1)    # hidden activations +1 for bias
+        num_correct = 0.0
+
+        for (x, target) in zip(x_test, y_test):
+            x = x.flatten()
+            x = np.insert(x, 0, 1.0)  # Adding bias input
+            self.forward(output_activations, hidden_activations, x)
+
+            # predict
+            prediction = np.argmax(output_activations)
+
+            if prediction == target:
+                num_correct += 1
+
+        return num_correct / len(x_test)
+
+    def confusion_matrix(self, x_test, y_test):
+        """
+        Create a confusion matrix on the test set
+        """
+        output_activations = np.zeros(self.output_units)        # output activations
+        hidden_activations = np.zeros(self.hidden_units + 1)    # hidden activations +1 for bias
+        y_true = []
+        y_pred = []
+
+        for (x, target) in zip(x_test, y_test):
+            x = x.flatten()
+            x = np.insert(x, 0, 1.0)  # Adding bias input
+            self.forward(output_activations, hidden_activations, x)
+
+            # predict
+            prediction = np.argmax(output_activations)
+
+            y_true.append(target)
+            y_pred.append(prediction)
+
+        return confusion_matrix(y_true, y_pred)
+
 
 def main(learning_rate, momentum, hidden_units, epochs):
     # load the MNIST dataset
@@ -156,8 +263,31 @@ def main(learning_rate, momentum, hidden_units, epochs):
     # Scale data to be between 0 and 1
     x_train, x_test = x_train / 255.0, x_test / 255.0
 
+    # Train
     perceptron = Perceptron(learning_rate, momentum, hidden_units)
-    perceptron.train(x_train, y_train, x_test, y_test, epochs)
+    train_accuracies, test_accuracies = perceptron.train(x_train, y_train, x_test, y_test, epochs)
+
+    # Plot
+    max_ticks = 10 # max ticks on x axis
+    step = max(1, len(train_accuracies) // max_ticks)
+    ticks = range(0, len(train_accuracies), step)
+
+    plt.plot(range(len(train_accuracies)), train_accuracies, label='train')
+    plt.plot(range(len(test_accuracies)), test_accuracies, label='test')
+    plt.ylim(0, 1)
+    plt.xticks(ticks, ticks)
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.title(f'Perceptron Learning Accuracy (η={learning_rate})')
+    plt.legend(loc='upper left')
+    plt.show()
+
+    # Confusion matrices for the perceptron on the test set
+    cm = perceptron.confusion_matrix(x_test, y_test)
+    display = ConfusionMatrixDisplay(confusion_matrix=cm)
+    display.plot()
+    plt.title(f'Perceptron Confusion Matrix on Test Set (η={learning_rate})')
+    plt.show()
 
     return 0
 
